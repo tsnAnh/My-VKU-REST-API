dc805d7edec397e54a4dbf5aabb5ad600c844ad1
tsnanh <tsnanh0812@gmail.com>
2020-04-11 11:13:19 +0700

    update 0.1.1-dev03

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,18 @@ const upload = multer({
   dest: "../uploads",
 });
 
-router.get('/user/is_user_registered', firebaseMiddleware.auth, async (req, res) => {
-  const user = await User.findOne({uid: res.locals.user.uid});
-  if (user != null) {
-    res.json(true);
-  } else {
-    res.json(false);
+router.get(
+  "/user/is_user_registered",
+  firebaseMiddleware.auth,
+  async (req, res) => {
+    const user = await User.findOne({ uid: res.locals.user.uid });
+    if (user != null) {
+      res.json(true);
+    } else {
+      res.json(false);
+    }
   }
-});
+);
 
 router.post("/uploads", upload.any(), async (req, res) => {
   try {
@@ -72,85 +76,90 @@ router.post("/user/new_user", firebaseMiddleware.auth, async (req, res) => {
 router.get("/t/create", function (req, res) {
   res.send("Hello There");
 });
-router.post("/t/create", firebaseMiddleware.auth, async (req, res, next) => {
-  const user = await User.findOne({uid: res.locals.user.uid});
-  res.locals.dbUser = user;
-  next();
-}, async (req, res) => {
-  try {
-    const requestThread = req.body.thread;
-    console.log(requestThread);
-    const requestPost = req.body.post;
-    console.log(requestPost);
-    const userId = res.locals.dbUser._id;
+router.post(
+  "/t/create",
+  firebaseMiddleware.auth,
+  async (req, res, next) => {
+    const user = await User.findOne({ uid: res.locals.user.uid });
+    res.locals.dbUser = user;
+    next();
+  },
+  async (req, res) => {
+    try {
+      const requestThread = req.body.thread;
+      console.log(requestThread);
+      const requestPost = req.body.post;
+      console.log(requestPost);
+      const userId = res.locals.dbUser._id;
 
-    const thread = new Thread({
-      _id: new mongoose.Types.ObjectId(),
-      title: requestThread.title,
-      image: requestThread.image,
-      forum_id: requestThread.forum_id,
-      user_id: userId,
-      user_avatar: res.locals.photoUrl,
-      user_display_name: res.locals.dbUser.display_name,
-    });
+      const thread = new Thread({
+        _id: new mongoose.Types.ObjectId(),
+        title: requestThread.title,
+        image: requestThread.image,
+        forum_id: requestThread.forum_id,
+        user_id: userId,
+        user_avatar: res.locals.dbUser.photoURL,
+        user_display_name: res.locals.dbUser.display_name,
+      });
 
-    console.log(thread);
-    console.log("User Avatar", res.locals.user);
+      console.log(thread);
+      console.log("User Avatar", res.locals.user);
 
-    const post = new Post({
-      _id: new mongoose.Types.ObjectId(),
-      content: requestPost.content,
-      user_id: userId,
-      thread_id: thread._id,
-    });
+      const post = new Post({
+        _id: new mongoose.Types.ObjectId(),
+        content: requestPost.content,
+        user_id: userId,
+        thread_id: thread._id,
+      });
 
-    thread.save(async (error) => {
-      if (error) {
-        throw error;
-      }
-      try {
-        await Forum.findOneAndUpdate(
-          { _id: requestThread.forum_id },
-          {
-            $inc: {
-              number_of_posts: 1,
-              number_of_threads: 1,
-            },
+      thread.save(async (error) => {
+        if (error) {
+          throw error;
+        }
+        try {
+          await Forum.findOneAndUpdate(
+            { _id: requestThread.forum_id },
+            {
+              $inc: {
+                number_of_posts: 1,
+                number_of_threads: 1,
+              },
+              $push: {
+                threads: thread._id,
+              },
+              last_updated_on: Date.now(),
+            }
+          );
+          await User.findOneAndUpdate(
+            { _id: userId },
+            {
+              $inc: {
+                number_of_threads: 1,
+              },
+              $push: {
+                threads: thread._id,
+                posts: post._id,
+              },
+            }
+          );
+          await thread.updateOne({
             $push: {
-              threads: thread._id,
-            },
-            last_updated_on: Date.now(),
-          }
-        );
-        await User.findOneAndUpdate(
-          { _id: userId },
-          {
-            $inc: {
-              number_of_threads: 1,
-            },
-            $push: {
-              threads: thread._id,
               posts: post._id,
             },
-          }
-        );
-        await thread.updateOne({
-          $push: {
-            posts: post._id,
-          },
-        });
-        await post.save();
-      } catch (e) {
-        throw e;
-      }
-    });
+          });
+          await post.save();
+        } catch (e) {
+          throw e;
+        }
+      });
 
-    res.json(thread);
-  } catch (e) {
-    console.log(e);
-    res.status(400).json("Something went wrong");
-    throw e;
+      res.json(thread);
+    } catch (e) {
+      console.log(e);
+      res.status(400).json("Something went wrong");
+      throw e;
+    }
   }
-});
+);
 
 module.exports = router;
