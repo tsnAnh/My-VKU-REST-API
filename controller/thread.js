fbe8476eb023c8cf70edbf7d5ab5595ed2d88bcc
tsnanh <tsnanh0812@gmail.com>
2020-05-09 15:03:56 +0700

    update 0.1.1-dev03

--- controller/thread.js
+++ controller/thread.js
@@ -1,6 +1,7 @@
+const mongoose = require('mongoose');
+
 const Thread = require('../schema/Thread.module');
 const User = require('../schema/User.module');
-const mongoose = require('mongoose');
 const Forum = require('../schema/Forum.module');
 const Post = require('../schema/User.module');
 
